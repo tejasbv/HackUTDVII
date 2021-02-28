686c038d73dd2f1f086603398d302ef88a5b37bb
tejas <tejas.bv2001@gmail.com>
2021-02-28 01:58:54 -0600

    homeUI background update

--- texas_relief/lib/homeUI.dart
+++ texas_relief/lib/homeUI.dart
@@ -8,70 +8,82 @@ import 'griddashboard (1).dart';
 class HomeState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /**********************************************************/
+    // full screen width and height
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    var padding = MediaQuery.of(context).padding;
+    // height without status and toolbar
+    double height3 = (height - kToolbarHeight) / 2;
+    double boxw = height > width ? width : height - 50;
+    /**********************************************************/
     return Scaffold(
       backgroundColor: Colors.blue[900],
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blue[700],
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.blue[900],
-            Colors.black,
-          ]),
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 60,
+      body: Stack(
+        children: [
+          Container(
+            child: Image(
+              image: AssetImage("assets/images/sky.png"),
+              fit: BoxFit.cover,
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 16, right: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Texas Relief",
-                        style: GoogleFonts.openSans(
-                            textStyle: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold)),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        "Home",
-                        style: GoogleFonts.openSans(
-                            textStyle: TextStyle(
-                                color: Color(0xffa29aac),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600)),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    alignment: Alignment.topCenter,
-                    icon: Image.asset(
-                      "assets/images/setting.png",
-                      width: 80,
-                      //color: Colors.whpurpte,
-                    ),
-                    onPressed: () {
-                      context.read<AuthenticationService>().signOut();
-                    },
-                  )
-                ],
+            height: height3 / 1.5,
+            width: width,
+          ),
+          Column(
+            children: <Widget>[
+              SizedBox(
+                height: 60,
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            GridDashboard()
-          ],
-        ),
+              Padding(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Texas Relief",
+                          style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "Home",
+                          style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                  color: Color(0xffa29aac),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      alignment: Alignment.topCenter,
+                      icon: Image.asset(
+                        "assets/images/setting.png",
+                        width: 80,
+                        //color: Colors.whpurpte,
+                      ),
+                      onPressed: () {
+                        context.read<AuthenticationService>().signOut();
+                      },
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              GridDashboard()
+            ],
+          ),
+        ],
       ),
     );
   }
